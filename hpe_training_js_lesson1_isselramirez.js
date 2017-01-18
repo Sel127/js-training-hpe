7a7ec5c72e166c680b6e1554b49ba1ca7be0699d
Sel127 <issel.ramirez@gmail.com>
2017-01-18 12:04:01 -0600

    HPE_training_lesson1
    issel's practice 1.

---
+++ hpe_training_js_lesson1_isselramirez.js
@@ -0,0 +1,33 @@
+/* This is HPE JavaScript training
+lesson 1 */
+
+// printing a phrase
+console.log("You gotta live and learn, you gotta crash and burn, You gotta take some stances and take some chances You gotta laugh and love and take all life has to give You gotta live and learn so you can learn to live. Though I left home and I went to Carolina I then set off just to see what I could see I did some things that I was not really proud of And sometimes life just got the best of me");
+
+//practicing length
+"practice".length
+"one".length
+"primera practica de issel".length
+"aprendiendo javascript".length
+"preacticando length".length
+
+//variables
+var variableText = "texto1";
+var variableNum1 = 23;
+var varTrue = true;
+var variableNum2 = 8;
+var variableNombre = "Issel";
+
+//interacting with user
+prompt("What's your name?");
+prompt("what's your age? ");
+prompt("is this practice easy?");
+prompt("do you like videogames?");
+prompt("do you like pizza?");
+
+//Arrays
+var nombres = ["Martha","Cristina","Jose"];
+var tallas = [5, 7, 9, 11];
+var varios = [34, "vaso", 11, "plato"];
+var colores = ["azul", "amarillo", "verde", "rojo"];
+var baterias = ["AA", "AAA", "D", "C"];
